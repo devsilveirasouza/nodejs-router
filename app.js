505e3b78d68eaab435b7cb23ea52b7913e665c4f
devsilveirasouza <dev.silveirasouza@mail.com>
2024-09-15 08:11:13 -0300

    feat(package): Nodemon added and config

--- app.js
+++ app.js
@@ -7,16 +7,22 @@ const app = express();
 const PORT = 3000;
 
 // DEFINIR EJS como motor de visualização
+// Configurando a pasta 'views' e a engine EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // MIDDLEWARE
+// COnfiguração do body-parser
 app.use(bodyParser.urlencoded({ extend: true }));
+
+// Configurando a pasta pública para os arquivos estáticos (CSS)
 app.set(express.static(path.join(__dirname, 'public')));
 
 // ROTAS
+// Rotas de produtos
 app.use('/', productRoutes);
 
+// Rodando o servidor
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
